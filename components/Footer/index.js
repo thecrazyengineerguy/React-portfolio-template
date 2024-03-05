40ad2fd138ef890178535d0efed18be1b21f7e83
Aman Kalal <amankalal206@gmail.com>
2024-03-05 21:35:48 +0530

    Edited my own data

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -25,8 +25,8 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
+        <Link  className="underline underline-offset-1" href="http://www.chetanverma.com">
+          Chetan Verma
         </Link>
       </h1>
     </>

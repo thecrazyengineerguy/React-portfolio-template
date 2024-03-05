c029a608bca0ef9c01d864bd59c0bcac6b6291f8
Aman Kalal <amankalal206@gmail.com>
2024-03-05 23:34:09 +0530

    Second Commit

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -26,7 +26,7 @@ const Footer = ({}) => {
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
         <Link  className="underline underline-offset-1" href="http://www.chetanverma.com">
-          Chetan Verma
+          Aman Kalal
         </Link>
       </h1>
     </>
